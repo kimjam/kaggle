6991e351b0711e0aff26fb48ae9d6e7f22e0963a
kimjam <jamesykim10@gmail.com>
2016-05-07 21:34:38 -0400

    testing flow

--- kobe/kobe_build.R
+++ kobe/kobe_build.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(caret)
+library(MASS)
 
 # minimize this
 multilogloss <- function(act, pred)
@@ -17,7 +18,7 @@ multilogloss <- function(act, pred)
 
 df <- read.csv('~/kaggle/kobe/data.csv')
 
-df$shot_made_flag <- factor(df$shot_made_flag, level = c('1', '0'))
+df$shot_made_flag <- factor(df$shot_made_flag, levels = c('1', '0'))
 
 # minutes played
 minutes_played <- (
@@ -26,6 +27,7 @@ minutes_played <- (
     df$period*5*60, 
     df$period*12*60)- df$minutes_remaining*60 - df$seconds_remaining) / 60
 
+df$minutes_played <- minutes_played
 # assume each game starts at midnight to add time
 # can only use data that happened before shot to predict shot
 df$game_date <- as.POSIXct(
@@ -35,13 +37,47 @@ df$game_date <- as.POSIXct(
   lubridate::minutes(floor(minutes_played)) + 
   lubridate::seconds((minutes_played %% 1)*60)
   
+set.seed(24) #kobe
 train <- df[!is.na(df$shot_made_flag), ]
-test <- df[is.na(df$shot_made_flag), ]
+test <- train[sample(1:nrow(train), 4000), ]
+train <- train[!(train$shot_id %in% test$shot_id), ]
+validate <- df[is.na(df$shot_made_flag), ]
 
 train_act <- cbind(ifelse(train$shot_made_flag == 1, 1, 0), 
                    ifelse(train$shot_made_flag == 0, 1, 0))
+
+test_act <- cbind(ifelse(test$shot_made_flag == 1, 1, 0), 
+                  ifelse(test$shot_made_flag == 0, 1, 0))
 
 # create another test set from training set
 # for each shot in new test set, create data set to train model
 # data set can only contain data before the shot you are trying to predict
 # logistic, random forest, svm, ensemble?
+formula <- shot_made_flag ~ combined_shot_type + shot_zone_area + shot_distance + minutes_played
+predictions <- data.frame(log = numeric(nrow(test)), 
+                          svm = numeric(nrow(test)), 
+                          rf = numeric(nrow(test)))
+
+logreg_build <- function(train, test, formula) {
+  logreg <- vector('numeric', nrow(test))
+  for (i in 1:nrow(test)) {
+    pre_df <- train %>%
+      dplyr::filter(
+        game_date <= test$game_date[i]
+      )
+    
+    logreg_model <- stepAIC(
+      glm(formula = formula, 
+          data = pre_df, 
+          family = binomial(link='logit')), 
+      direction = 'backward', 
+      trace = FALSE)
+    
+    logreg[i] <- predict(logreg_model, newdata = test[i, ], type = 'response')
+  }
+  
+  return(logreg)
+}
+
+predictions$log <- logreg_build(train = train, test = test, formula = formula)
+
